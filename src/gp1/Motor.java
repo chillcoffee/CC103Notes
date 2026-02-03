26e32036e4098c7a05b24fbc52a6690ed80ba3dc
Ruffa <ruffaresentes@me.com>
2026-02-03 16:05:24 +0800

    added final code for Motor (gatepass 1)

--- src/gp1/Motor.java
+++ src/gp1/Motor.java
@@ -1,6 +1,29 @@
 package gp1;
 
 public class Motor {
+    //attributes
+    String brand = "Honda";
+    double price = 64000;
     
+    //constructor
+    public Motor(){
+        /*
+        codes here to initialize object
+        */
+    }
+    
+    public static void main(String[] args) {
+        //instantiation = create object
+        //Classname objectName = new Classname();
+        Motor honda = new Motor();  //instantiation
+        System.out.print("I will buy a "+honda.brand+" worth ");
+        System.out.println(String.format("%.2f",honda.price)+" pesos.");
+        honda.start();  //method call
+    }
+    
+    //methods
+    public void start(){
+        System.out.println("Motor is starting...");
+    }
     
 }
