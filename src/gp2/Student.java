6d7c5d81857f4b76e38cc4980d6af71a2c1c4a79
Ruffa <ruffaresentes@me.com>
2026-02-12 10:26:32 +0800

    create gp2 for SM

---
+++ src/gp2/Student.java
@@ -0,0 +1,12 @@
+package gp2;
+public class Student {
+    
+    private String name;
+    private int subjects;
+    private float tuition;
+
+    //create constructor, getter, setter
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
